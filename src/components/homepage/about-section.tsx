7f10dcfd956e004d53452c86ec1af225fd89e84f
krishtina <krishtinamaharjan8@gmail.com>
2025-04-04 22:24:35 +0545

    about section completed

--- src/components/homepage/about-section.tsx
+++ src/components/homepage/about-section.tsx
@@ -1,9 +1,44 @@
-export default function AboutSection(){
-    return(
-        <div className="container border-2 border-red-600">
-        <div className="text-center">
-            <h1 className="italic text-2xl font-bold">About Inline Skates</h1>
+import Image from "next/image";
+
+export default function AboutSection() {
+  return (
+    <div className="bg-body mt-[34px] p-24">
+      <div className="container ml-auto mr-auto ">
+        <div className="flex items-center justify-center">
+          <h1 className="italic  mb-[51px] text-4xl font-bold">
+            About Inline Skates
+          </h1>
         </div>
+        <div className=" grid gap-4 lg:grid-cols-3">
+          <div>
+            <div className="">
+              <Image
+                src="/assets/image1.png"
+                alt="shoe"
+                width={389}
+                height={346}
+              />
+            </div>
+          </div>
+          <div className="">
+            <Image
+              src="/assets/image2.png"
+              alt="shoe"
+              width={389}
+              height={346}
+            />
+          </div>
+          <div className="">
+            <Image
+              src="/assets/image3.png"
+              alt="shoe"
+              width={389}
+              height={346}
+            />
+          </div>
         </div>
-    );
+
+      </div>
+    </div>
+  );
 }
